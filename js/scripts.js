d5b71bcd8220f374506da2b1272f860ab93950c6
Zoltán Umlauf <zoltan.umlauf@outlook.com>
2022-05-31 19:53:58 +0200

    Initial commit with 2022 resume

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function reveal(cls, tokens) {
+    var field = document.getElementsByClassName(cls)[0].innerHTML;
+    for (var i=0; i<tokens.length; i++) {
+        var token = tokens[i].split('=');
+        var regex = new RegExp(' \\|' + token[0] + '\\| ', 'g');
+        field = field.replace(regex, token[1]);
+    }
+    document.getElementsByClassName(cls)[0].innerHTML = field;
+}
+
+window.onload = function() {
+    reveal('mail', ['a=@', 'm=gmail', 'd=.']);
+}
